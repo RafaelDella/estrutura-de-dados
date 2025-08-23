d751168b06ab573c8b60a48ee48da9a5b8b35c0a
Rafael Della Giustina <62893087+RafaelDella@users.noreply.github.com>
2025-08-22 22:25:09 -0300

    preparando pila e fila

---
+++ estrutura_de_dados/src/lista_encadeada/MinhaColuna.java
@@ -0,0 +1,26 @@
+package lista_encadeada;
+
+public class MinhaColuna {
+    //node temp
+
+    //push
+    public  void Adicionar(){
+
+    }
+
+    //pop
+    public void Remover(){
+
+    }
+
+    //Clear
+    public void Limpar(){
+
+    }
+
+    //isEmpty
+    public boolean isVazio(){
+        //if size != 0  return true
+        return false;
+    }
+}
---
+++ estrutura_de_dados/src/lista_encadeada/MinhaFila.java
@@ -0,0 +1,30 @@
+package lista_encadeada;
+
+public class MinhaFila {
+    MinhaLista<String> listaFrutas = new MinhaLista<>();
+    //noede first
+    //node topo
+
+    //enqueue
+    public  void Adicionar(){
+
+    }
+
+    //deenqueue
+    public void Remover(){
+
+    }
+
+    //Clear
+    public void Limpar(){
+
+    }
+
+    //isEmpty
+    public boolean isVazio(){
+        //if size != 0  return true
+        return false;
+    }
+
+
+}
